ed7863e24a6a65ac22db753aa5ae73f1a8a29ad5
Márcio Fontes <marcio@marcio-K52Jc.(none)>
2015-04-01 15:30:29 +0100

    Deleting old files

--- proj/src/InputFunction.java
+++
@@ -1,9 +0,0 @@
-import java.io.Serializable;
-
-abstract public class InputFunction implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-	//abstract public double getOutput(Connection[] inputs);
-	
-}
